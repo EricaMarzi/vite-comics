60f70d1c928d1f90c07cad72a924ba3b9f0c25da
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-02 18:48:07 +0100

    added card app main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,18 +7,42 @@ export default {
 <template>
     <main class="container">
         <div class="row">
+            <div class="col">
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
 
-            <div class="card">
-
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
             </div>
         </div>
     </main>
 </template>
 
 <style scoped>
+.row {
+    padding: 50px 0;
+    width: 100%;
+}
+
+.col {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 30px;
+    margin: 0 -1rem;
+}
+
 .card {
-    width: 100px;
-    height: 100px;
+    margin: 1ren;
+    width: calc(100% / 6 - 2rem);
+    height: 190px;
     background-color: white;
 }
 </style>
